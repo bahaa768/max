5f37c147c0d36b1c69aaa1a297079153e03aa137
bahaa mohamed <bahaa768@gmail.com>
2018-10-16 00:28:33 +0300

    lecture 78 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  featureLoaded = 'recipe';
+  onNavigate(feature: string){
+    this.featureLoaded = feature;
+  }
 }
