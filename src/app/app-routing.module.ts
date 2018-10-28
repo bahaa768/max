3b50151d5f1c2ab10892233aec77dc45858b02e8
bahaa moahmed <bahaa768@gmail.com>
2018-10-28 22:47:21 +0300

    after deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent, pathMatch: 'full'},
     { path: 'new', component: RecipeEditComponent},
@@ -16,6 +16,7 @@ const appRoutes: Routes = [
     { path: ':id/edit', component: RecipeEditComponent},
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
+  { path: ':something', redirectTo: 'recipes' }
 
 ];
 
